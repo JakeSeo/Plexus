8d7b1836c091d712b06a6a3fc5f88f89484a5db6
Winona Louise <winona_erive@dlsu.edu.ph>
2017-03-20 22:22:07 +0800

    Ito napo naayos ko na po hh cleaner

--- household_manager/views.py
+++ household_manager/views.py
@@ -6,7 +6,7 @@ from household_manager.forms import DocumentForm
 from household_manager.models import Document, FileManagerDocument
 import os
 import os.path, time
-
+import io
 
 def index(request):
     context = {}
@@ -51,6 +51,7 @@ def choose(request):
             newfile.document.name = "households/" + original_name + extension
             newfile.doc_name = original_name
             newfile.save()
+            cleanHH(original_name)
             return redirect('household_manager:choose')
     else:
         form = DocumentForm()
@@ -60,3 +61,104 @@ def choose(request):
         'filemanager_filenames': household_documents
     }
     return render(request, 'household_manager/File-Manager.html', context)
+
+def cleanHH(source):
+    f = io.open("media/households/" + source + "_cleaned.json", 'w', encoding="utf-8")
+    f.write("[\n")
+    ctr = 0
+    index = -1
+    with io.open("media/households/" + source + ".json", encoding="utf-8") as z:
+        for line in z:
+            data = json.loads(line)
+            if ctr != 0:
+                f.write(",\n")
+            else:
+                ctr = ctr + 1
+            index = index + 1
+            f.write("{\"id\":\"")
+            f.write(data['_id']['$oid'])
+            f.write("\", \"latitude\":\"")
+            if data['geopoint_hh']['latitude'] is None or data['geopoint_hh']['latitude'] is "":
+                f.write(str(0))
+            else:
+                f.write(data['geopoint_hh']['latitude'])
+            f.write("\", \"longitude\":\"")
+            if data['geopoint_hh']['longitude'] is None or data['geopoint_hh']['longitude'] is "":
+                f.write(str(0))
+            else:
+                f.write(data['geopoint_hh']['longitude'])
+            f.write("\", \"phsize\":\"")
+            if data['phsize'] is None:
+                f.write(str(0))
+            else:
+                f.write(str(data['phsize']))
+            f.write("\", \"car\":\"")
+            if data['car'] is None:
+                f.write(str(0))
+            else:
+                f.write(str(data['car']))
+            f.write("\", \"motor\":\"")
+            if data['motor'] is None:
+                f.write(str(0))
+            else:
+                f.write(str(data['motor']))
+            f.write("\", \"landagri\":\"")
+            if data['landagri'] is None:
+                f.write(str(0))
+            else:
+                f.write(str(data['landagri']))
+            f.write("\", \"landres\":\"")
+            if data['landres'] is None:
+                f.write(str(0))
+            else:
+                f.write(str(data['landres']))
+            f.write("\", \"landcomm\":\"")
+            if data['landcomm'] is None:
+                f.write(str(0))
+            else:
+                f.write(str(data['landcomm']))
+            f.write("\", \"salind\":\"")
+            if data['salind'] is None:
+                f.write(str(0))
+            else:
+                f.write(data['salind'])
+            f.write("\", \"servind\":\"")
+            if data['servind'] is None:
+                f.write(str(0))
+            else:
+                f.write(data['servind'])
+            f.write("\", \"trnind\":\"")
+            if data['trnind'] is None:
+                f.write(str(0))
+            else:
+                f.write(data['trnind'])
+            f.write("\", \"minind\":\"")
+            if data['minind'] is None:
+                f.write(str(0))
+            else:
+                f.write(data['minind'])
+            f.write("\", \"totin\":\"")
+            if data['totin'] is None:
+                f.write(str(0))
+            else:
+                f.write(str(data['totin']))
+            educind = 0
+            jobind = 0
+            fjob = 0
+            for x in data['hpq_mem']:
+                if x['educind'] is '1':
+                    educind = educind + 1
+                if x['jobind'] is '1':
+                    jobind = jobind + 1
+                if x['fjob'] is '1':
+                    fjob = fjob + 1
+            f.write("\", \"toteduc\":\"")
+            f.write(str(educind))
+            f.write("\", \"totjob\":\"")
+            f.write(str(jobind))
+            f.write("\", \"totfjob\":\"")
+            f.write(str(fjob))
+            f.write("\"}")
+    f.write("\n]")
+    f.close()
+    z.close()
