a69ce01c2cb268d1c9c0dfa2e8b63546588ebdfe
JakeSeo <jsds4927@yahoo.com>
2017-03-23 05:19:53 +0900

    fixed the flow of the system

--- landuse_manager/urls.py
+++ landuse_manager/urls.py
@@ -4,7 +4,7 @@ from . import views
 app_name = 'landuse_manager'
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^manage/$', views.manage, name='manage'),
-    url(r'^manage/save/$', views.manageSave, name="manageSave"),
+    url(r'^manage/(?P<filename>\w+.json)/$', views.manage, name='manage'),
+    url(r'^manage/(?P<filename>\w+.json)/save/$', views.manageSave, name="manageSave"),
     url(r'^choose/$', views.choose, name='choose'),
 ]
